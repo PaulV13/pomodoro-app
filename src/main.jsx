cae7e3710e708857425fd03108303fe98276c032
Paul Vidart <paulvidart@gmail.com>
2022-04-23 19:44:35 -0300

    Semana 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ModeContextProvider } from './contexts/ModeContext'
 import { PomodoroContextProvider } from './contexts/PomodoroContext'
@@ -11,13 +11,13 @@ import App from './App'
 ReactDOM.render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
-			<BrowserRouter>
+			<HashRouter>
 				<ModeContextProvider>
 					<PomodoroContextProvider>
 						<App />
 					</PomodoroContextProvider>
 				</ModeContextProvider>
-			</BrowserRouter>
+			</HashRouter>
 		</ChakraProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
